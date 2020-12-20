10e2e28e6bac65bf7e13ea4bd66c00a06000332b
Touyama Hiroto <e205708@ie.u-ryukyu.ac.jp>
2020-12-20 11:58:31 +0900

    add description? javadoc を追加した！

--- app/src/main/java/jp/ac/uryukyu/ie/e205708/Word.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205708/Word.java
@@ -1,18 +1,43 @@
 package jp.ac.uryukyu.ie.e205708;
 
+/**
+ * this class is used as Word in wordbook.
+ */
 public class Word {
+    /**
+     * String front is first displaying word basically English.
+     */
     private String front;
+
+    /**
+     * String back is second displaying word basically Japanese.
+     */
     private String back;
 
+    /**
+     * constructer
+     * @param _front 
+     * @param _back 
+     */
     Word(String _front,String _back){
         this.front = _front;
         this.back = _back;
     }
 
+    /**
+     * this mathod display String front.
+     * 
+     * @see WordList#studyWords()
+     */
     public void callFront(){
         System.out.println(this.front);
     }
 
+    /**
+     * this mathod display String back.
+     * 
+     * @see WordList#studyWords()
+     */
     public void callBack(){
         System.out.println(this.back);
     }
--- app/src/main/java/jp/ac/uryukyu/ie/e205708/WordList.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205708/WordList.java
@@ -3,14 +3,44 @@ package jp.ac.uryukyu.ie.e205708;
 import java.io.*;
 import java.util.ArrayList;
 
+/**
+ * This class is used as wordbook.
+ * This class have "Word" class List. and can make arraylist from specific file.
+ */
 public class WordList {
+    /**
+     * filename : The String is used to search file. 
+     */
     String filename;
 
+    /**
+     * constructor
+     * @param _filename the file name you want to study
+     */
     WordList(String _filename){
         this.filename = _filename;
     }
+
+    /**
+     * wordList : ArrayList of Word.
+     * @see Word makeWordList()
+     */
     ArrayList<Word> wordList = new ArrayList<Word>();
 
+    /**
+     * this mathod makes Word Object from specific file and add these to wordList.
+     * ""warning"" : this method make BufferedReader from "/Users/touyuamahiroto/prog2/report6/app/arc/main/resources/" + filename in default.
+     *             : you have to change these your environment.
+     *             :
+     *             : file format - english:translation
+     *             :               another_english:translation
+     * 
+     * references
+     * https://www.javadrive.jp/start/stream/index3.html
+     * すっきりわかるjava入門第３版,p679,try-with-resources文
+     * 
+     * @see Word
+     */
     public void makeWordList(){
         try(BufferedReader br = new BufferedReader(new FileReader("/Users/touyamahiroto/prog2/report6/app/src/main/resources/"+filename))){
             String str = "";
@@ -30,6 +60,16 @@ public class WordList {
         } 
     }
 
+    /**
+     * you can study english.
+     * 1.displaying english.
+     * 2.stop moment.
+     * 3.displaying the word translation.
+     * 
+     * lasting this moving to last word.
+     * 
+     * @see waitMoment callFront() callBack()
+     */
     public void studyWords(){
         System.out.println("スタート\n\n");
 
@@ -45,7 +85,14 @@ public class WordList {
         System.out.println("お疲れ様でした！");
     }
 
-    //prog_exe2で書かれてたものをそのまま持ってきてます。
+    /**
+     * this mathod is used in studyWords() to stop motion.
+     * 
+     * @param time : assign time to stop.
+     * 
+     * quotation
+     * watMoment at Actionクラスの装飾 in https://docs.google.com/document/d/1n_LWLTqTeDz-oJmVgQu5A_JNbPMEuB0V1PZ8Rxz29YU/edit
+     */
     public void waitMoment(int time){
         try{
             Thread.sleep(time);
